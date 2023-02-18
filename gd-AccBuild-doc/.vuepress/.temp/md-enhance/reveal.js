668aa669c2e231690f4fcfb44ab64cb0abe41b6e
gd771747384 <771747384@qq.com>
2023-02-18 21:35:17 +0800

    完善

--- gd-AccBuild-doc/.vuepress/.temp/md-enhance/reveal.js
+++
@@ -1,4 +0,0 @@
-import { reveal, revealMarkdown, revealHighlight, revealMath, revealSearch, revealNotes, revealZoom } from "F:/MyWorkSpace_program/lowcode-nocode/gd-AccBuild-doc/node_modules/.pnpm/vuepress-plugin-md-enhance@2.0.0-beta.97/node_modules/vuepress-plugin-md-enhance/lib/client/reveal";
-
-export const useReveal = () => [reveal(), revealMarkdown(), revealHighlight(), revealMath(), revealSearch(), revealNotes(), revealZoom()
-];
